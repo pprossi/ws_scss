959095c5b08489fe2f3f83a8bbb4587c45b47d86
pprossi <rrsp@unitybox.de>
2019-10-25 09:17:37 +0200

    Fix: backend break on backend stylesheet inclusion
    Load render-preProcess only for frontend

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,7 +3,9 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess']['wsscss'] = 'WapplerSystems\\WsScss\\Hooks\\RenderPreProcessorHook->renderPreProcessorProc';
+if (TYPO3_MODE === 'BE') {
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess']['wsscss'] = 'WapplerSystems\\WsScss\\Hooks\\RenderPreProcessorHook->renderPreProcessorProc';
+}
 
 // Caching the pages - default expire 3600 seconds
 if (!is_array($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['ws_scss'])) {
